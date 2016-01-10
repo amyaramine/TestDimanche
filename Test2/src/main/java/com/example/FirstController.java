4fa6bdeb52396f2876aaae00533d74300d82caff
amyaramine <amyaramine@gmail.com>
2016-01-10 17:17:26 +0100

    Modificaiton in the controller and template

--- Test2/src/main/java/com/example/FirstController.java
+++ Test2/src/main/java/com/example/FirstController.java
@@ -1,13 +1,15 @@
 package com.example;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FirstController {
 
 	@RequestMapping("/")
-	public String 	affichage(){
+	public String 	affichage(Model m){
+		m.addAttribute("hi", "helloow");
 		return "simple";
 	}
 }
